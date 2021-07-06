0d641a0e655ebc0ca6c9ea6888a5880dfae81826
saboalina <79441551+saboalina@users.noreply.github.com>
2021-07-06 11:06:04 +0300

    Add files via upload

---
+++ mpp-proiect-repository-saboalina-Lab3C-/Laborator2C/repository/IAngajatRepository.cs
@@ -0,0 +1,11 @@
+﻿using Lab2C.domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2C.repository
+{
+    interface IAngajatRepository : IRepository<string, Angajat>
+    {
+    }
+}
